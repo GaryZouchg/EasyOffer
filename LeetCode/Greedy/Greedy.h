55e1997cbae91744ab67b689eabf0149f4dc1cd8
Gary <5227025305@qq.com>
2019-01-18 13:09:00 +0800

    greedy candy

--- LeetCode/Greedy/Greedy.h
+++ LeetCode/Greedy/Greedy.h
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
@@ -8,6 +10,16 @@ public:
 	static vector<int> static_gas;
 	static vector<int> static_cost;
 	int gasStation(vector<int> &gas = static_gas, vector<int> &cost=static_cost);
+
+	static vector<int> static_ratings;
+	int candy(vector<int> &ratings= static_ratings);
+
+
+	static string static_word;
+	static unordered_set<string> static_dict;
+	int wordBreak1( string word = static_word, unordered_set<string> &dict = static_dict );
+	int wordBreak2( string word = static_word, unordered_set<string> &dict = static_dict );
+
 };
 // sample for removeElement
 
---
+++ LeetCode/Greedy/WordBreak.cpp
@@ -0,0 +1,3 @@
+#include "stdafx.h"
+#include "Greedy.h"
+
