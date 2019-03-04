26d2a432eb6c8f10394d92761caeeb5c749d0907
garyzouchg <522702305@qq.com>
2019-03-04 21:09:22 +0800

    misc  big  little endian

---
+++ CPP_Base/Misc/Misc.cpp
@@ -0,0 +1,31 @@
+// Misc.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int isLittleEndian()
+{
+	//–°∂À–Ú£∫ µÕµÿ÷∑µÕŒª   
+	unsigned short num = 0x1234;
+	if (*((char*)&num)==0x34)
+	{
+		return 1;
+	}
+	return 0;
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	unsigned short i_num = 0x1234;
+	if (isLittleEndian())
+		std::cout << "little endian . " << std::endl;
+	else
+		std::cout << "big endian . " << std::endl;
+
+
+	system("pause");
+	return 0;
+}
+
