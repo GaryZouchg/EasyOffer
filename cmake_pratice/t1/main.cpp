2d2058247cd9b667d5e8ba73918786fc37b7ea01
garyzouchg <522702305@qq.com>
2019-03-07 19:08:06 +0800

    cmake pratice

---
+++ cmake_pratice/t1/main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main()
+{
+	std::cout<<"Hello World from t1 Main() !   "<<std::endl;
+	
+	return 0;
+}
---
+++ cmake_pratice/t2/src/main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main()
+{
+	std::cout<<"Hello World from t2 main.cpp main() !   "<<std::endl;
+	
+	return 0;
+}
